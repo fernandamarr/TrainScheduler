1d5e8c971f2e411d57a9a5a000a30315c09b3b7f
Fernanda Marroquin <fernanda.marroquin07@gmail.com>
2018-12-17 18:59:03 -0500

    recent updates js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,6 @@
 // Users from many different machines must be able to view same train times. 
 // provide up-to-date info with moment.js -- arrival time and how many minutes remain until arrival times
 
-
 $(document).ready(function () {
 
     // Initialize Firebase
@@ -21,7 +20,7 @@ $(document).ready(function () {
     var database = firebase.database();
 
     // Initial values
-    var train = "";
+    var name = "";
     var destination = "";
     var time = "03:30";
     var frequency = 3;
@@ -31,14 +30,39 @@ $(document).ready(function () {
         event.preventDefault();
 
         // Store and retrieve most recent input
-        train = $("#train").val().trim();
+        name = $("#name").val().trim();
         destination = $("#destination").val().trim();
-        frequency = $("#frequency").val().trim();
         time = $("#time").val().trim();
+        frequency = $("#frequency").val().trim();
+
+        // Manipulate train time with moment.js
+        //first time
+        var timeConverted = moment(time, "HH:mm").subtract(1, "years");
+        console.log(timeConverted);
+
+        // curent time
+        var currentTime = moment();
+        console.log("current time: " + moment(currentTime).format("hh:mm a"));
+
+        // difference between the times
+        var diffTime = moment().diff(moment(timeConverted), "minutes");
+        console.log("Difference in time: " + diffTime);
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % frequency;
+        console.log(tRemainder);
+
+        // Minute until train
+        var minutesUntilTrain = frequency - tRemainder;
+        console.log("Minutes until train: " + minutesUntilTrain);
+
+        // Next train
+        var nextTrain = moment().add(minutesUntilTrain, "minutes");
+        console.log("Arrival time: " + moment(nextTrain).format("hh:mm"));
 
         // Change what is saved in firebase
         database.ref().set({
-            train: train,
+            name: name,
             destination: destination,
             time: time,
             frequency: frequency
@@ -48,19 +72,17 @@ $(document).ready(function () {
     // Add changes to HTML
     database.ref().on("value", function (snapshot) {
 
-        // // Print initial data to console
+        // Print initial data to console
         console.log(snapshot.val());
-        console.log(snapshot.val().train);
+        console.log(snapshot.val().name);
         console.log(snapshot.val().destination);
         console.log(snapshot.val().time);
         console.log(snapshot.val().frequency);
 
-        // time = moment(time).format('HH:mm');
-
         // Change HTML
-        $("tbody").append("<tr><td><strong>" + snapshot.val().train + "</strong></td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().frequency + "</td><td>" + snapshot.val().time + "</td></tr>");
+        $("tbody").append("<tr><td><strong>" + snapshot.val().name + "</strong></td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().frequency + "</td><td>" + snapshot.val().time + "</td></tr>");
 
-        // Log errors to console
+        // Handle errors
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
     })
