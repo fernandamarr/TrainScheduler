e66bd55d94bc8f0f506f456f1511798b9b17d133
Fernanda Marroquin <fernanda.marroquin07@gmail.com>
2018-12-17 13:10:42 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// When adding trains, administrators should be able to submit the following:
+// Train Name
+// Destination 
+// First Train Time -- in military time
+// Frequency -- in minutes
+// Code this app to calculate when the next train will arrive; this should be relative to the current time.
+// Users from many different machines must be able to view same train times. 
+
+// Incorporates Firebase to host arrival and departure data. 
+// Retrieve and manipulate this information with Moment.js. 
+// This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
