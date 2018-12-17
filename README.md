2a37105b608ec200b25eced1bbc02053ddecbdb5
Fernanda Marroquin <fernanda.marroquin07@gmail.com>
2018-12-17 16:39:24 -0500

    edits to all files

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# TrainScheduler
-A train schedule application that incorporates Firebase to host arrival and departure data
+<h1>Train Scheduler</h1>
+
+A train schedule application that incorporates Firebase to host arrival and departure data. This website provides up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+
+<h2>Technologies Used</h2>
+
+* HTML
+* CSS/Bootstrap
+* JavaScript
+* jQuery
+* Firebase
+* Moment.js
+
+<h2>Getting Started</h2>
+
+* Administrators are able to submit the following information:
+  * Train Name
+  * Destination
+  * First Train Time -- in military time
+  * Frequency -- in minutes
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,67 @@
-// When adding trains, administrators should be able to submit the following:
-// Train Name
-// Destination 
-// First Train Time -- in military time
-// Frequency -- in minutes
-// Code this app to calculate when the next train will arrive; this should be relative to the current time.
+// manipulate train time in military time with moment.js
+// frequency in minutes and first train time in military time
 // Users from many different machines must be able to view same train times. 
+// provide up-to-date info with moment.js -- arrival time and how many minutes remain until arrival times
 
-// Incorporates Firebase to host arrival and departure data. 
-// Retrieve and manipulate this information with Moment.js. 
-// This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+
+$(document).ready(function () {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDZJPGsLd5yx4hoVyUKstt2XrP77rIAt3I",
+        authDomain: "trainscheduler-79921.firebaseapp.com",
+        databaseURL: "https://trainscheduler-79921.firebaseio.com",
+        projectId: "trainscheduler-79921",
+        storageBucket: "trainscheduler-79921.appspot.com",
+        messagingSenderId: "66428144576"
+    };
+    firebase.initializeApp(config);
+
+    // Variable to reference the database
+    var database = firebase.database();
+
+    // Initial values
+    var train = "";
+    var destination = "";
+    var time = "03:30";
+    var frequency = 3;
+
+    // Capture button click
+    $("#add-train").on("click", function (event) {
+        event.preventDefault();
+
+        // Store and retrieve most recent input
+        train = $("#train").val().trim();
+        destination = $("#destination").val().trim();
+        frequency = $("#frequency").val().trim();
+        time = $("#time").val().trim();
+
+        // Change what is saved in firebase
+        database.ref().set({
+            train: train,
+            destination: destination,
+            time: time,
+            frequency: frequency
+        });
+    });
+
+    // Add changes to HTML
+    database.ref().on("value", function (snapshot) {
+
+        // // Print initial data to console
+        console.log(snapshot.val());
+        console.log(snapshot.val().train);
+        console.log(snapshot.val().destination);
+        console.log(snapshot.val().time);
+        console.log(snapshot.val().frequency);
+
+        // time = moment(time).format('HH:mm');
+
+        // Change HTML
+        $("tbody").append("<tr><td><strong>" + snapshot.val().train + "</strong></td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().frequency + "</td><td>" + snapshot.val().time + "</td></tr>");
+
+        // Log errors to console
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    })
+});
